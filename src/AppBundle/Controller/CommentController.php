93646f2d0e65a1411f6a8e0e9863c1bcdd4d510c
t_prokop <tomas.prokop@sportgate.cz>
2016-11-20 16:14:43 +0100

    aktivace komentaru u editace prispevku

---
+++ src/AppBundle/Controller/CommentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CommentController
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/post/{postId}/comments")
+     * @Template("comments/list.html.twig")
+     */
+    public function getCommentsAction($postId)
+    {
+        $comments = $this->em->getRepository(Comment::class)->findBy(['post' => $postId]);
+
+        return ['comments' => $comments];
+    }
+}
